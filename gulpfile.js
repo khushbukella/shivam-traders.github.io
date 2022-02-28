0ae81d815d7f54a37edc22c83c364950098a0e75
khushbukella <khushbukellan@gmail.com>
2022-02-28 17:20:14 +0530

    >

--- gulpfile.js
+++ gulpfile.js
@@ -197,3 +197,15 @@ gulp.task('netlify', gulp.series(
   'images:netlify:build',
   'plugins:netlify:build'
 ));
+
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
